e7c446bf8a8f7fc5beca7db4cf2448de4461f84c
duxiaobo <nickdu2009@gmail.com>
2019-05-19 18:47:50 +0800

    validate bson objectid

--- xvalidator/custom.go
+++ xvalidator/custom.go
@@ -1,4 +1,21 @@
 package xvalidator
 
+import (
+	"github.com/gin-gonic/gin/binding"
+	"github.com/globalsign/mgo/bson"
+	"gopkg.in/go-playground/validator.v9"
+)
+
 func RegisterCustomValidator() {
+	v := binding.Validator.Engine().(*validator.Validate)
+	v.RegisterValidation("is-objectid", ValidateObjectId)
+}
+
+func ValidateObjectId(fl validator.FieldLevel) bool {
+	if idStr, ok := fl.Field().Interface().(string); ok {
+		if bson.IsObjectIdHex(idStr) {
+			return true
+		}
+	}
+	return false
 }
